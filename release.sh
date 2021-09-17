e7172f7cee408d529cc9b9036df5cb277d36dae5
michaelzhao <michaelpublic@qq.com>
2021-09-17 16:13:58 +0800

    batch release

--- release.sh
+++ release.sh
@@ -5,15 +5,18 @@ usage()
     echo "Usage: $0 [usw|hk]"
 }
 
-REGISTRY="$1ccr.ccs.tencentyun.com"
-IMAGE="onemore/librad"
-VERSION=$(cat VERSION)
-SOURCE=${IMAGE}:${VERSION}
-TARGET=${REGISTRY}/${SOURCE}
+while [ $# -gt 0 ]; do
+    REGISTRY="$1ccr.ccs.tencentyun.com"
+    IMAGE="onemore/librad"
+    VERSION=$(cat VERSION)
+    SOURCE=${IMAGE}:${VERSION}
+    TARGET=${REGISTRY}/${SOURCE}
 
-read -p "Release '${TARGET}', are you sure? (y/n)" -n 1 -r yn
-echo
-[ "x$yn" != "xy" ] && exit 1
+    read -p "Release '${TARGET}', are you sure? (y/n)" -n 1 -r yn
+    echo
+    [ "x$yn" != "xy" ] && exit 1
 
-docker tag ${SOURCE} ${TARGET} && docker push ${TARGET}
+    docker tag ${SOURCE} ${TARGET} && docker push ${TARGET}
 
+    shift
+done
