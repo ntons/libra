7a2b8edb904cdaf37b9cdb296800b602cbe3683b
michaelzhao <michaelpublic@qq.com>
2023-06-26 13:24:38 +0800

    bugfix for multiple topic read

--- librad/services/pubsub/pub_sub.go
+++ librad/services/pubsub/pub_sub.go
@@ -6,9 +6,11 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/ntons/libra-go"
 	v1pb "github.com/ntons/libra-go/api/libra/v1"
+	"github.com/ntons/log-go"
 	"github.com/ntons/redis"
 	"google.golang.org/protobuf/proto"
 )
@@ -86,76 +88,73 @@ func (*pubSubServer) Subscribe(
 		return
 	}
 
-	// 当前会话的读取位置，重建会话时需要调用者提供起始位置
-	cursors := make(map[string]string)
+	var (
+		wg sync.WaitGroup // reading goroutine waitgroup
+		mu sync.Mutex     // sending mutex
+	)
+	defer wg.Wait()
+
+	// 多个topic可能存在不同散列，需要分开read
 	for topic, start := range req.TopicStart {
 		id := fmt.Sprintf("%d-0", start.SinceMilliTimestamp)
 		if start.AfterId != "" {
 			a := strings.SplitN(start.AfterId, "-", 2)
-			if len(a) != 2 {
+			if v, err := strconv.ParseInt(a[0], 10, 64); err != nil {
 				return newInvalidArgumentError(
 					"invalid start after id: %v", start.AfterId)
-			}
-			var v int64
-			if v, err = strconv.ParseInt(a[0], 10, 64); err != nil {
-				return newInvalidArgumentError(
-					"invalid start after id: %v", start.AfterId)
-			}
-			if _, err = strconv.ParseInt(a[1], 10, 64); err != nil {
-				return newInvalidArgumentError(
-					"invalid start after id: %v", start.AfterId)
-			}
-			if v >= start.SinceMilliTimestamp {
-				id = id
+			} else if v >= start.SinceMilliTimestamp {
+				id = start.AfterId
 			}
 		}
-		cursors[toStream(appId, topic)] = id
+		wg.Add(1)
+		go func(args *redis.XReadArgs) {
+			defer wg.Done()
+			for {
+				r, err := cli.XRead(stream.Context(), args).Result()
+				if err != nil {
+					log.Warnf("failed to read pubsub message: %v", err)
+					return
+				}
+				var resp = &v1pb.PubSub_SubscribeResponse{}
+				for _, e := range r {
+					for _, m := range e.Messages {
+						args.Streams[1] = m.ID
+						v, ok := m.Values["PubSubMsg"]
+						if !ok {
+							log.Warnf("failed to parse pubsub message: %v", err)
+							continue
+						}
+						var b []byte
+						if b, err = base64.StdEncoding.DecodeString(v.(string)); err != nil {
+							log.Warnf("failed to decode pubsub message: %v", err)
+							continue
+						}
+						msg := &v1pb.PubSub_Message{}
+						if err = proto.Unmarshal(b, msg); err != nil {
+							log.Warnf("failed to unmarshal pubsub message: %v", err)
+							continue
+						}
+						msg.Topic = toTopic(e.Stream)
+						msg.Id = m.ID
+						resp.Msgs = append(resp.Msgs, msg)
+					}
+				}
+				if len(resp.Msgs) == 0 {
+					continue
+				}
+				mu.Lock()
+				err = stream.Send(resp)
+				mu.Unlock()
+				if err != nil {
+					log.Warnf("failed to send pubsub response: %v", err)
+					return
+				}
+			}
+		}(&redis.XReadArgs{
+			Streams: []string{toStream(appId, topic), id},
+			Count:   int64(req.BatchCount),
+		})
 	}
 
-	for {
-		args := &redis.XReadArgs{}
-		for stream, _ := range cursors {
-			args.Streams = append(args.Streams, stream)
-		}
-		for _, cursor := range cursors {
-			args.Streams = append(args.Streams, cursor)
-		}
-		if req.BatchCount > 0 {
-			args.Count = int64(req.BatchCount)
-		}
-
-		var resp = &v1pb.PubSub_SubscribeResponse{}
-
-		var r []redis.XStream
-		if r, err = cli.XRead(stream.Context(), args).Result(); err != nil {
-			return
-		}
-		for _, e := range r {
-			for _, m := range e.Messages {
-				cursors[e.Stream] = m.ID
-				v, ok := m.Values["PubSubMsg"]
-				if !ok {
-					continue
-				}
-				var b []byte
-				if b, err = base64.StdEncoding.DecodeString(v.(string)); err != nil {
-					continue
-				}
-				msg := &v1pb.PubSub_Message{}
-				if err = proto.Unmarshal(b, msg); err != nil {
-					continue
-				}
-				msg.Topic = toTopic(e.Stream)
-				msg.Id = m.ID
-				resp.Msgs = append(resp.Msgs, msg)
-			}
-		}
-		if len(resp.Msgs) == 0 {
-			continue
-		}
-		if err = stream.Send(resp); err != nil {
-			return
-		}
-	}
 	return
 }
